007c36efcccaf4105c04eb256d2e1ea229290dd9
周旭 <zhouxusoft@gmail.com>
2023-03-23 01:07:00 +0800

    完善部分功能

--- public/main/script.js
+++ public/main/script.js
@@ -3,22 +3,34 @@ let socket = new WebSocket("ws://localhost:3008");
 
 //拿到登录的token
 let token = JSON.parse(localStorage.getItem("token"))
+console.log(token)
 //判断值是否为空
 if (!token) {
     window.location = '../login/'
 }
 
+//获取到输出框，用于修改内容
+let output = document.getElementsByClassName("output");
+
 // 当连接打开时，发送一条消息给服务器
 socket.onopen = function(e) {
-    socket.send("已连接");
+    output[0].innerHTML += `<p class="joinin">${token.nickname} 已连接</p>`;
 };
 
 // 当收到服务器的消息时，将它显示在网页上
 socket.onmessage = function(event) {
+    //将字符串转回对象
     console.log(event.data)
-    let output = document.getElementsByClassName("output");
-    output[0].innerHTML += `<p class="username">${token.username}</p>`;
-    output[0].innerHTML += `<p class="message">${event.data}</p>`;
+    let data = JSON.parse(event.data)
+    if (data.nickname == token.nickname) {
+        output[0].innerHTML += `<div class="selfuser">${data.nickname}</div>`;
+        output[0].innerHTML += `<div class="selfmessage">${data.value}</div>`;
+        output[0].innerHTML += `<div class="clear"></div>`;
+    } else {
+        output[0].innerHTML += `<div class="otheruser">${data.nickname}</div>`;
+        output[0].innerHTML += `<div class="othermessage">${data.value}</div>`;
+    }
+    output[0].scrollTop = output[0].scrollHeight;
 };
 
 // 当连接关闭时，显示一个提示信息
@@ -32,9 +44,13 @@ socket.onclose = function(event) {
 
 sendForm.addEventListener('submit', function (e) {
     e.preventDefault()
-    let toSend = this.tosend.value
-    socket.send(toSend)
-    console.log(toSend)
+    let toSend = {nickname: token.nickname, value: this.tosend.value}
+    if (toSend.value) {
+        socket.send(JSON.stringify(toSend))
+    }
+    console.log(toSend.nickname)
+    //清空input
+    document.getElementsByClassName("input")[0].value = "";
 })
 // 当发生错误时，显示一个警告信息
 socket.onerror = function(error) {
--- server.js
+++ server.js
@@ -4,14 +4,16 @@ const WebSocket = require('ws');
 // 创建一个WebSocket服务器实例，监听3000端口
 const wss = new WebSocket.Server({ port: 3008 });
 
+let onlineCount = 0;
+
 // 当有客户端连接时，触发connection事件
 wss.on('connection', function connection(ws) {
   // 当收到客户端消息时，触发message事件
   ws.on('message', function incoming(message) {
-    console.log(message);
     // 把消息原样返回给客户端
-    
+    wss.emit("B",message.toString())
     ws.send(message.toString());
+    console.log(message.toString());
   });
 
 });

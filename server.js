3fade8bc7049f5e28b216efb93b8c4d87bfda3ea
周旭 <zhouxusoft@gmail.com>
2023-03-26 01:46:00 +0800

    ..

--- server.js
+++ server.js
@@ -5,28 +5,6 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-//导入用户路由模块
-const userRouter = require('./router/user')
-
-//导入cors中间件
-const cors = require('cors')
-
-//将cors注册为全局中间件
-app.use(cors())
-
-//在所有路由之前封装一个res.cc()函数
-app.use((req, res, next) => {
-    //status默认值为1，表示失败
-    //err的值，可以是一个错误对象，也可以是一个错误描述字符串
-    res.cc = function(err, status = 1) {
-        res.send({status, message: err instanceof Error ? err.message : err})
-    }
-    next()
-})
-
-//全局挂载用户路由
-app.use('/api', userRouter)
-
 //默认加载页面
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/main/');
